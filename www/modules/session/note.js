44fb8ac0c56cb604ef01a25a5d2e7df9b6bc0d6e
gqueste <kakashi1hatake>
2016-02-19 14:39:05 +0100

    Cordova Camera : get picture from gallery or camera and insert into SQLite

--- www/modules/session/note.js
+++ www/modules/session/note.js
@@ -1,7 +1,25 @@
 angular.module('conf.session')
-    .controller('noteController', ['$scope', '$sce', '$cordovaSQLite', function($scope, $sce, $cordovaSQLite){
+    .controller('noteController', ['$scope', '$sce', '$cordovaSQLite', '$cordovaCamera', function($scope, $sce, $cordovaSQLite, $cordovaCamera){
+        $scope.session = app.navi.getCurrentPage().options.session;
+        $scope.session.photos = [];
 
         var db = $cordovaSQLite.openDB({ name: "conference.db" });
+
+        var loadPhotos = function(){
+            var query = "CREATE TABLE IF NOT EXISTS session_photos (id text, url text)";
+            $cordovaSQLite.execute(db, query, []).then(function(res){
+                var selectQuery = "SELECT * from session_photos where id=?";
+                $cordovaSQLite.execute(db, selectQuery, [$scope.session.id]).then(function(res){
+                    for(var i = 0; i < res.rows.length; i++){
+                        $scope.session.photos.push(res.rows.item(i).url);
+                    }
+                }, function(err){
+                    console.error(err);
+                });
+            }, function(err){
+                console.error(err);
+            });
+        };
 
         var loadNote = function() {
             var query = "CREATE TABLE IF NOT EXISTS session_notes (id text primary key, notes text)";
@@ -14,6 +32,7 @@ angular.module('conf.session')
                     else {
                         $scope.session.notes = "Mes notes";
                     }
+                    loadPhotos();
                 }, function(err){
                     console.error(err);
                 });
@@ -22,8 +41,6 @@ angular.module('conf.session')
             });
         };
 
-
-        $scope.session = app.navi.getCurrentPage().options.session;
         loadNote();
 
         var insertNote = function(id, notes){
@@ -36,6 +53,58 @@ angular.module('conf.session')
 
         $scope.enregistrerNote = function(){
             insertNote($scope.session.id, $scope.session.notes);
+        };
+
+        var savePhoto = function(photo){
+            var query = "INSERT OR REPLACE INTO session_photos VALUES (?,?)";
+            $cordovaSQLite.execute(db, query, [$scope.session.id, photo]).then(function(res){
+
+            }, function(err){
+                console.error(err);
+            });
+        };
+
+        var enregistrerImage = function(options){
+            $cordovaCamera.getPicture(options).then(function(imageURI) {
+                $scope.session.photos.push(imageURI);
+                savePhoto(imageURI);
+            }, function(err) {
+                // error
+            });
+        };
+
+        $scope.addPhotoFromLibrary = function () {
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                allowEdit: false,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 100,
+                targetHeight: 100,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false,
+                correctOrientation:false
+            };
+
+            enregistrerImage(options);
+        };
+
+        $scope.addPhotoFromCamera = function(){
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 100,
+                targetHeight: 100,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: true,
+                correctOrientation:false
+            };
+
+            enregistrerImage(options);
         }
 
     }]);
